f994a9799d904c5a63c7eadefdc99559f2a52544
sally <sallyakrum83@gmail.com>
2019-04-13 13:17:57 -0500

    solve the map and fillter js

--- 01-Activities/09-Stu_FunctionalLoops/Unsolved/map.js
+++ 01-Activities/09-Stu_FunctionalLoops/Unsolved/map.js
@@ -10,8 +10,19 @@ console.log(originalArray);
 // Complete the following
 
 // 1. A map that triples the originalArray and sets the result equal to a new `tripledArray` array
-
+const tripledArray = originalArray.map(function(data){
+  data*3
+});
 // 2. A map that takes the originalArray and returns a new array `oddOrEven`
+const oddOrEven = originalArray.map(function(num){
+  if(num %2 === 0){
+    console.log("even");
+  
+  }
+  else{
+    console.log("odd");
+  }
+})
 // containing the text "even" if the number is even and the text "odd" if the number is odd
 // i.e. using this map, the array [3, 1, 2, 4] should give us back ["odd", "odd", "even", "even"]
 
